e7ec768a6b1d620f6b36562db233b0b667aeeb22
Niraj Kamdar <nirajkamdar27@pm.me>
2026-01-01 14:04:25 +0400

    wip: deployment config

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.19;
 
 import {MockV3Aggregator} from "../test/mock/MockV3Aggregator.sol";
 import {Script, console2} from "forge-std/Script.sol";
+import {Config} from "forge-std/Config.sol";
 
 abstract contract CodeConstants {
     uint8 public constant DECIMALS = 8;
@@ -16,7 +17,7 @@ abstract contract CodeConstants {
     uint256 public constant LOCAL_CHAIN_ID = 31337;
 }
 
-contract HelperConfig is CodeConstants, Script {
+contract HelperConfig is CodeConstants, Script, Config {
     /*//////////////////////////////////////////////////////////////
                                  ERRORS
     //////////////////////////////////////////////////////////////*/
@@ -43,9 +44,14 @@ contract HelperConfig is CodeConstants, Script {
         networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getSepoliaEthConfig();
         networkConfigs[ZKSYNC_SEPOLIA_CHAIN_ID] = getZkSyncSepoliaConfig();
         // Note: We skip doing the local config
+
+        // Load config from deployments.toml
+        _loadConfig("./deployments.toml", false);
     }
 
-    function getConfigByChainId(uint256 chainId) public returns (NetworkConfig memory) {
+    function getConfigByChainId(
+        uint256 chainId
+    ) public returns (NetworkConfig memory) {
         if (networkConfigs[chainId].priceFeed != address(0)) {
             return networkConfigs[chainId];
         } else if (chainId == LOCAL_CHAIN_ID) {
@@ -58,16 +64,24 @@ contract HelperConfig is CodeConstants, Script {
     /*//////////////////////////////////////////////////////////////
                                 CONFIGS
     //////////////////////////////////////////////////////////////*/
-    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
-        return NetworkConfig({
-            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306 // ETH / USD
-        });
+    function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
+        address priceFeed = config.get("price_feed").toAddress();
+        return
+            NetworkConfig({
+                priceFeed: priceFeed // ETH / USD
+            });
     }
 
-    function getZkSyncSepoliaConfig() public pure returns (NetworkConfig memory) {
-        return NetworkConfig({
-            priceFeed: 0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF // ETH / USD
-        });
+    function getZkSyncSepoliaConfig()
+        public
+        view
+        returns (NetworkConfig memory)
+    {
+        address priceFeed = config.get("price_feed").toAddress();
+        return
+            NetworkConfig({
+                priceFeed: priceFeed // ETH / USD
+            });
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -82,7 +96,10 @@ contract HelperConfig is CodeConstants, Script {
         console2.log(unicode"⚠️ You have deployed a mock contract!");
         console2.log("Make sure this was intentional");
         vm.startBroadcast();
-        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
         vm.stopBroadcast();
 
         localNetworkConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
